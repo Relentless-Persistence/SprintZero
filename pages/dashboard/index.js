ab6673501765b331093e7515759bc0f564bf1fc5
Irene-24 <okaforirenen@gmail.com>
2022-02-10 20:40:49 +0100

    dashboard fix

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import DashboardLayout from "../../components/Dashboard/AppLayout";
+import DashboardLayout from "../../components/Dashboard/Layout";
+
 
 export default function Home ()
 {
