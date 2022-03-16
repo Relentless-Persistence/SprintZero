afdd52700a2ecc4225a2e1bb15244593e91bfb29
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-03-16 19:00:51 +0100

    Minimal viewport

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,12 @@ import { RecoilRoot } from "recoil";
 
 function MyApp ( { Component, pageProps } )
 {
+  if (typeof window !== "undefined") {
+    const w = window.innerWidth;
+    if (w < 1198) {
+      document.body.innerHTML = `<div class="w-full h-full flex justify-center items-center text-lg p-8">Sorry! We don't work on this device size yet, but we're working on it! Please share this link with a device that has a resolution above 1194 px | iPad Air and above</div>`;
+    } 
+  }
   return (
     <AuthProvider>
       <RecoilRoot>
