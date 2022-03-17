80e5b6610d0fb90f3830b6d7ea659add6154dca9
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-03-17 13:55:41 +0100

    Added react-use

--- pages/_app.js
+++ pages/_app.js
@@ -5,22 +5,21 @@ import "../styles/globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
 import { PaymentProvider } from "../contexts/PaymentContext";
 import { RecoilRoot } from "recoil";
+import { useLayoutEffect, useState, useEffect } from "react";
+import { useWindowSize } from "react-use";
 
-
-
-function MyApp ( { Component, pageProps } )
-{
-  if (typeof window !== "undefined") {
-    const w = window.innerWidth;
-    if (w < 1198) {
+function MyApp({ Component, pageProps }) {
+  const { width } = useWindowSize();
+  
+    if (width < 1198) {
       document.body.innerHTML = `<div class="w-full h-full flex justify-center items-center text-lg p-8">Sorry! We don't work on this device size yet, but we're working on it! Please share this link with a device that has a resolution above 1194 px | iPad Air and above</div>`;
-    } 
-  }
+    }
+
   return (
     <AuthProvider>
       <RecoilRoot>
         <PaymentProvider>
-          <Component { ...pageProps } />
+          <Component {...pageProps} />
         </PaymentProvider>
       </RecoilRoot>
     </AuthProvider>
