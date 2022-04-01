58ef196967e9652ff21594fef6911011097c8c98
irene@relentlesspersistenceinc.com <IreneRPInc>
2022-04-01 12:06:09 +0100

    bug fixes for cards

--- components/Personas/TimeLineCard.js
+++ components/Personas/TimeLineCard.js
@@ -15,6 +15,18 @@ import { PlusCircleOutlined, MinusCircleOutlined } from '@ant-design/icons';
 import CardHeaderButton from "../Dashboard/CardHeaderButton";
 import ActionButtons from "./ActionButtons";
 
+const MyCard = styled( Card )`
+
+
+    .ant-card-head
+    {
+        min-height:unset;
+        border-bottom: 2px solid #D9D9D9;
+    }
+
+`;
+
+
 const MyInput = styled( Input )`
 
     .ant-input-group-wrapper
@@ -51,6 +63,8 @@ const Remove = styled( Button )`
     padding:0;
     display: flex;
     align-items: center;
+
+    
 `;
 
 
@@ -58,13 +72,14 @@ const TimeLineCard = (
     {
         handleEdit,
         title = "Goals",
-        name,
-        cardData = [ "" ]
+        cardData = []
     }
 ) => 
 {
 
     const [ form ] = Form.useForm();
+    const [ list, setList ] = useState( [ ...cardData ] );
+
 
     const [ isEdit, setIsEdit ] = useState( false );
 
@@ -72,145 +87,161 @@ const TimeLineCard = (
 
     const onFinish = () =>
     {
-        const values = form.getFieldsValue();
-        handleEdit( values[ `${ name }${ title }fields` ].filter( ( v ) => v?.trim().length > 0 ) );
-        toggleEdit();
+        const values = list.filter( it => it.trim().length > 0 );
+        handleEdit( values );
+        setIsEdit( false );
+    };
+
+
+    const onCancel = () =>
+    {
+        setIsEdit( false );
+    };
+
+    const add2 = () =>
+    {
+        const newList = [ ...list, "", "" ];
+        setList( newList );
+    };
+
+    const add = ( index ) =>
+    {
+        if ( index )
+        {
+            const beforeIndex = [ ...list ].slice( 0, index );
+            const after = [ ...list ].slice( index );
+            setList( [ ...beforeIndex, "", ...after ] );
+        }
+        else
+        {
+            const newList = [ ...list, "" ];
+            setList( newList );
+        }
 
     };
+
+    const remove = ( index ) =>
+    {
+        const newList = list.filter( ( _, i ) => i !== index );
+        setList( newList );
+    };
+
+    const onChange = ( e, i ) =>
+    {
+        const newList = [ ...list ];
+        newList[ i ] = e.target.value;
+
+        setList( newList );
+    };
+
 
     if ( isEdit )
     {
 
+        const first = list[ 0 ];
+        const last = list[ list.length - 1 ];
+        const beforeLast = list[ list.length - 2 ];
+
+        const rest = list.slice( 1, -2 );
+
         return (
-            <Card
+            <MyCard
                 className="border-2 border-[#D9D9D9]"
                 extra={ <ActionButtons
-                    onCancel={ toggleEdit }
+                    onCancel={ onCancel }
                     onSubmit={ onFinish }
                 /> }
                 title={ <strong>{ title }</strong> }
                 headStyle={ {
                     background: "#F5F5F5",
                 } }>
-                <Form form={ form }>
-                    <Form.List
-                        initialValue={ cardData }
-                        name={ `${ name }${ title }fields` }>
-                        {
-                            ( fields, { add, remove } ) =>
-                            {
-                                const first = fields[ 0 ];
-                                const last = fields[ fields.length - 1 ];
-                                const beforeLast = fields[ fields.length - 2 ];
 
-                                const rest = fields.slice( 1, -2 );
+                <MyInput
+                    placeholder={ "Start | 6 am" }
+                    value={ first }
+                    $hasLeftBorder={ true }
+                    $removeRightBorder={ list.length === 1 }
+                    className="mb-[12px]"
+                    onChange={ ( e ) => onChange( e, 0 ) }
 
+                    addonAfter={ list.length < 2 ?
+                        <Add
+                            onClick={ add2 }
+                        >
+                            <PlusCircleOutlined
+                                style=
+                                { {
+                                    color: "#009C7E"
+                                } } />
+                        </Add> : null } />
 
-                                return (
-                                    <div>
-                                        <Form.Item
-                                            key={ 0 }
-                                            name={ [ 0 ] }
-                                            rules={ [ { required: true } ] }
-                                        >
+                {
+                    list.length >= 3 ? <>
+                        { rest.map( ( field, index ) => (
+                            <MyInput
+                                className="mb-[12px]"
+                                onChange={ ( e ) => onChange( e, index + 1 ) }
 
-                                            <MyInput
-                                                placeholder={ "Start" }
-                                                value={ first }
-                                                $hasLeftBorder={ true }
-                                                $removeRightBorder={ fields.length === 1 }
-                                                addonAfter={ fields.length < 2 ?
-                                                    <Add
-                                                        onClick={ () => { add(); add(); } }
-                                                    >
-                                                        <PlusCircleOutlined
-                                                            style=
-                                                            { {
-                                                                color: "#009C7E"
-                                                            } } />
-                                                    </Add> : null }
-                                            />
-                                        </Form.Item>
+                                key={ index + 1 }
+                                addonAfter={
+                                    <Add
+                                        onClick={ () => add( index + 2 ) }
+                                    >
+                                        <PlusCircleOutlined
+                                            style=
+                                            { {
+                                                color: "#009C7E"
+                                            } } />
+                                    </Add> }
+                                addonBefore={ <Remove onClick={ () => remove( index + 1 ) }>
+                                    <MinusCircleOutlined style=
+                                        { {
+                                            color: "#C82D73"
+                                        } } /></Remove> }
+                                placeholder={ "Next Step" }
+                                value={ field }
+                            />
+                        ) ) }
 
-                                        {
-                                            fields.length >= 3 ?
-                                                <>
+                        <MyInput
+                            key={ list.length - 2 }
+                            onChange={ ( e ) => onChange( e, list.length - 2 ) }
+                            placeholder={ "Next step" }
+                            value={ beforeLast }
+                            $removeRightBorder={ true }
+                            className="mb-[12px]"
+                            addonBefore={ <Remove onClick={ remove }>
+                                <MinusCircleOutlined
+                                    style=
+                                    { {
+                                        color: "#C82D73"
+                                    } } /></Remove> }
+                            addonAfter={
+                                <Add
+                                    onClick={ () => add( list.length - 1 ) }
+                                >
+                                    <PlusCircleOutlined
+                                        style=
+                                        { {
+                                            color: "#009C7E"
+                                        } } />
+                                </Add> } />
 
-                                                    { rest.map( ( field, index ) => (
-                                                        <Form.Item
-                                                            key={ index + 1 }
-                                                            name={ [ index + 1 ] }
-                                                            rules={ [ { required: true } ] }
-                                                        >
+                    </> : null
+                }
 
-                                                            <MyInput
-                                                                addonBefore={ <Remove onClick={ () => remove( index ) }>
-                                                                    <MinusCircleOutlined style=
-                                                                        { {
-                                                                            color: "#C82D73"
-                                                                        } } /></Remove> }
-                                                                placeholder={ "Next Step" }
-                                                                value={ field[ index + 1 ] }
-                                                            />
-                                                        </Form.Item>
-                                                    ) ) }
+                {
+                    list.length > 2 ?
+                        <Input
+                            onChange={ ( e ) => onChange( e, list.length - 1 ) }
+                            className="mb-[12px]"
+                            key={ list.length - 1 }
+                            placeholder={ "End | 10 am" }
+                            value={ last } />
+                        : null
+                }
 
-
-                                                    <Form.Item
-                                                        key={ fields.length - 2 }
-                                                        name={ [ fields.length - 2 ] }
-                                                        rules={ [ { required: true } ] }
-                                                    >
-
-                                                        <MyInput
-                                                            placeholder={ "Next step" }
-                                                            value={ beforeLast }
-                                                            $removeRightBorder={ true }
-                                                            addonBefore={ <Remove onClick={ () => remove( fields.length - 2 ) }>
-                                                                <MinusCircleOutlined
-                                                                    style=
-                                                                    { {
-                                                                        color: "#C82D73"
-                                                                    } } /></Remove> }
-                                                            addonAfter={
-                                                                <Add
-                                                                    onClick={ () => add() }
-                                                                >
-                                                                    <PlusCircleOutlined
-                                                                        style=
-                                                                        { {
-                                                                            color: "#009C7E"
-                                                                        } } />
-                                                                </Add> } />
-                                                    </Form.Item>
-                                                </> : null
-                                        }
-
-                                        {
-                                            fields.length > 2 ? <Form.Item
-                                                key={ fields.length - 1 }
-                                                name={ [ fields.length - 1 ] }
-                                                rules={ [ { required: true } ] }
-                                            >
-
-                                                <Input
-                                                    placeholder={ "End" }
-                                                    value={ last } />
-                                            </Form.Item> : null
-                                        }
-
-                                    </div>
-
-
-                                );
-
-
-
-                            }
-                        }
-                    </Form.List>
-                </Form>
-            </Card>
+            </MyCard>
         );
 
     }
@@ -229,7 +260,7 @@ const TimeLineCard = (
                     cardData.map( ( item, i ) => (
                         <Timeline.Item key={ i } color=
                             {
-                                i == 0 ? "green" : "blue"
+                                i === 0 ? "green" : ( i === cardData.length - 1 ? "black" : "blue" )
                             }>
                             <p>{ item }</p>
                         </Timeline.Item>
