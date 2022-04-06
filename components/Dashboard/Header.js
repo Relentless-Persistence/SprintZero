aef672d83643251d5c1300c285f5bdce62bd3e21
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-04-06 13:56:16 +0100

    Header padding changed

--- components/Dashboard/Header.js
+++ components/Dashboard/Header.js
@@ -46,7 +46,7 @@ const AppHeader = ({ onChangeProduct }) => {
     >
       <div className="flex items-center">
         <Image
-          src="https://firebasestorage.googleapis.com/v0/b/sprintzero-657f3.appspot.com/o/Light.png?alt=media&token=ede37d0b-e499-4005-8bd7-7bca97b35235"
+          src="https://firebasestorage.googleapis.com/v0/b/sprintzero-657f3.appspot.com/o/logoDarkLettersWhite%201.png?alt=media&token=e97358f0-8ed9-4026-b9a7-42c224ff4d64"
           alt="Logo"
           className="w-[178px] h-[42px]"
           preview={false}
