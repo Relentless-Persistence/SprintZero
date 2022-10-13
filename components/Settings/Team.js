7ef5eaabb606822dbdc825fd229a81bc1c07c4e0
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-10-13 23:39:43 +0100

    Teams settings

--- components/Settings/Team.js
+++ components/Settings/Team.js
@@ -1,89 +1,202 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
-import { Card, Space, Button, Form, Input, Row, Col, Drawer, Radio, DatePicker, TimePicker, Switch } from "antd";
+import {
+  Card,
+  Space,
+  Button,
+  Form,
+  Input,
+  Row,
+  Col,
+  Drawer,
+  Radio,
+  DatePicker,
+  TimePicker,
+  Switch,
+  Tabs,
+} from "antd";
 import {
   DeleteOutlined,
   HourglassOutlined,
   PoweroffOutlined,
+  KeyOutlined
 } from "@ant-design/icons";
+import { db } from "../../config/firebase-config";
+import { activeProductState } from "../../atoms/productAtom";
+import { useRecoilValue, useRecoilState } from "recoil";
 
 const characters =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-const Team = () => {
+const Team = ({ teams }) => {
+  const activeProduct = useRecoilValue(activeProductState);
+  const [expiry, setExpiry] = useState("");
+  const [id, setId] = useState(null);
+
+  const handleDateChange = (date, dateString) => {
+
+    db.collection("teams")
+      .doc(id)
+      .update({
+        expiry: dateString
+      })
+  };
+
+  const removeTeam = (id) => {
+    db.collection("teams").doc(id).delete();
+  }
+
+  const activateTeam = (id) => {
+    db.collection("teams").doc(id).update({
+      expiry: "unlimited",
+    });
+  }
 
   return (
     <div id="billings">
       <div className="mb-[42px]">
-        <h3 className="font-semibold text-16 mb-3">Members</h3>
-        <Card>
-          <div className="flex items-center justify-between">
-            <div>
-              <p>Theresa Webb</p>
-              <p>twebb@example.com</p>
-            </div>
-            <Space>
-              <DatePicker placeholder="Unlimited" />
-              <Button className="flex items-center" icon={<DeleteOutlined />}>
-                Remove
-              </Button>
-            </Space>
-          </div>
-        </Card>
-        <Card className="mt-[9px]">
-          <div className="flex items-center justify-between">
-            <div>
-              <p>Theresa Webb</p>
-              <p>twebb@example.com</p>
-            </div>
-            <Space>
-              <DatePicker placeholder="Unlimited" />
-              <Button className="flex items-center" icon={<DeleteOutlined />}>
-                Remove
-              </Button>
-            </Space>
-          </div>
-        </Card>
-      </div>
-      <div>
-        <h3 className="font-semibold text-16 mb-3">Viewers</h3>
-        <Card className="mb-[9px]">
-          <div className="flex items-center justify-between">
-            <div>
-              <p>Theresa Webb</p>
-              <p>twebb@example.com</p>
-            </div>
-            <Space>
-              <DatePicker placeholder="Unlimited" />
-              <Button className="flex items-center" icon={<DeleteOutlined />}>
-                Remove
-              </Button>
-            </Space>
-          </div>
-        </Card>
-        <Card>
-          <div className="flex items-center justify-between">
-            <div>
-              <p>Theresa Webb</p>
-              <p>twebb@example.com</p>
-            </div>
-            <Space>
-              <Button
-                className="flex items-center"
-                icon={<HourglassOutlined />}
-                danger
-                ghost
-              >
-                Expired
-              </Button>
-              <Button className="flex items-center" icon={<PoweroffOutlined />}>
-                Reactivate
-              </Button>
-            </Space>
-          </div>
-        </Card>
-      </div>
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane
+            tab={<div className="font-semibold text-16">Members</div>}
+            key="1"
+          >
+            <>
+              {teams
+                .filter((item) => item.type === "member")
+                .filter(
+                  (item) =>
+                    item.expiry === "unlimited" ||
+                    new Date(item.expiry) < new Date()
+                )
+                .map((team, i) => (
+                  <Card key={i}>
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <p className="font-semibold">{team.user.name}</p>
+                        <p>{team.user.email}</p>
+                      </div>
+                      <Space>
+                        <DatePicker
+                          placeholder={team.expiry}
+                          onChange={handleDateChange}
+                          onClick={() => setId(team.id)}
+                        />
+                        <Button
+                          className="flex items-center"
+                          icon={<DeleteOutlined />}
+                          onClick={() => removeTeam(team.id)}
+                        >
+                          Remove
+                        </Button>
+                      </Space>
+                    </div>
+                  </Card>
+                ))}
+              <div className="font-semibold text-16 mt-4 mb-4">Archive</div>
+              {teams
+                .filter((item) => item.type === "member")
+                .filter((item) => new Date(item.expiry) > new Date())
+                .map((team, i) => (
+                  <Card key={i}>
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <p className="font-semibold">{team.user.name}</p>
+                        <p>{team.user.email}</p>
+                      </div>
+                      <Space>
+                        <Button
+                          className="flex items-center"
+                          icon={<HourglassOutlined />}
+                          danger
+                          ghost
+                        >
+                          Expired
+                        </Button>
+                        <Button
+                          className="flex items-center"
+                          icon={<KeyOutlined />}
+                          onClick={() => activateTeam(team.id)}
+                        >
+                          Activate
+                        </Button>
+                      </Space>
+                    </div>
+                  </Card>
+                ))}
+            </>
+          </Tabs.TabPane>
 
-      {/*  */}
+          <Tabs.TabPane
+            tab={<h3 className="font-semibold text-16">Viewers</h3>}
+            key="2"
+          >
+            <>
+              {teams
+                .filter((item) => item.type === "viewer")
+                .filter(
+                  (item) =>
+                    item.expiry === "unlimited" ||
+                    new Date(item.expiry) < new Date()
+                )
+                .map((team, i) => (
+                  <Card key={i} className="mb-[9px]">
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <p className="font-semibold">{team.user.name}</p>
+                        <p>{team.user.email}</p>
+                      </div>
+                      <Space>
+                        <DatePicker
+                          placeholder={team.expiry}
+                          onChange={handleDateChange}
+                          onClick={() => setId(team.id)}
+                        />
+                        <Button
+                          className="flex items-center"
+                          icon={<DeleteOutlined />}
+                          onClick={() => removeTeam(team.id)}
+                        >
+                          Remove
+                        </Button>
+                      </Space>
+                    </div>
+                  </Card>
+                ))}
+              <div className="font-semibold text-16 mt-4 mb-4">Archive</div>
+              {teams
+                .filter((item) => item.type === "viewer")
+                .filter((item) => new Date(item.expiry) > new Date())
+                .map((team, i) => (
+                  <Card key={i}>
+                    <div className="flex items-center justify-between">
+                      <div>
+                        <p className="font-semibold">{team.user.name}</p>
+                        <p>{team.user.email}</p>
+                      </div>
+                      <Space>
+                        <Button
+                          className="flex items-center"
+                          icon={<HourglassOutlined />}
+                          danger
+                          ghost
+                        >
+                          Expired
+                        </Button>
+                        <Button
+                          className="flex items-center"
+                          icon={<KeyOutlined />}
+                          onClick={() => activateTeam(team.id)}
+                        >
+                          Activate
+                        </Button>
+                      </Space>
+                    </div>
+                  </Card>
+                ))}
+            </>
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
     </div>
   );
 };
--- pages/dashboard/settings/team.js
+++ pages/dashboard/settings/team.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
@@ -7,13 +8,35 @@ import { useRouter } from "next/router";
 import Account from "../../../components/Settings/Account";
 import { findIndex } from "lodash";
 import Team from "../../../components/Settings/Team";
+import { db } from "../../../config/firebase-config";
+import { activeProductState } from "../../../atoms/productAtom";
+import { useRecoilValue, useRecoilState } from "recoil";
 
 const share = () => {
   const router = useRouter();
   const { pathname } = useRouter();
+  const activeProduct = useRecoilValue(activeProductState);
   const menus = ["Account", "Billing", "Config", "Team"];
   const [activeMenuIndex, setActiveMenuIndex] = useState(3);
   const [addPerson, setAddPerson] = useState(false);
+  const [teams, setTeams] = useState(null);
+
+  const fetchTeams = () => {
+    if (activeProduct) {
+      db.collection("teams")
+        .where("product_id", "==", activeProduct.id)
+        .onSnapshot((snapshot) => {
+          setTeams(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+          console.log(
+            snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+          );
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetchTeams();
+  }, [activeProduct]);
   
 
   const generateToken = () => {
@@ -52,7 +75,7 @@ const share = () => {
         activeRightItem={menus[activeMenuIndex]}
         setActiveRightNav={setActiveRightNav}
       >
-        <Team />
+        {teams && <Team teams={teams} />}
       </TeamLayout>
     </div>
   );
