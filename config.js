f97b5bb545e2c51fc102cf302a19057d8993ae05
Adil Gurbanov <adil.gurban@gmail.com>
2022-11-22 21:40:04 +0100

    installed dotenv and add config file

---
+++ config.js
@@ -0,0 +1,4 @@
+// import all env vars from .env file
+require('dotenv').config()
+export const NEXT_PUBLIC_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_PUBLISHABLE_KEY
+export const NEXT_PUBLIC_SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY
