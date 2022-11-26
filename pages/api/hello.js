20103a9152fdcf1b7646fe929d998907af8d4678
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-11-27 00:48:25 +0100

    Revert changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe', key: process.env.NEXT_PUBLIC_SECRET_KEY })
+  res.status(200).json({ name: 'John Doe' })
 }
