df188e83e00f3232f6a2a1825339f06d24b93681
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2022-12-01 22:59:24 +0100

    Add Status change notification to learnings

--- pages/dashboard/userbase/learnings.js
+++ pages/dashboard/userbase/learnings.js
@@ -1,213 +1,211 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { Radio, message, Empty } from "antd";
+import React, {useState, useEffect} from "react"
+import Head from "next/head"
+import {useRouter} from "next/router"
+import {Radio, message, Empty, notification} from "antd"
 
-import AppLayout from "../../../components/Dashboard/AppLayout";
-import {
-  ActionFormCard,
-  ActionFormCard2,
-} from "../../../components/Dashboard/FormCard";
-import ItemCard from "../../../components/Dashboard/ItemCard";
-import { splitRoutes, timeScale } from "../../../utils";
-import MasonryGrid from "../../../components/Dashboard/MasonryGrid";
-import { RadioButtonWithFill } from "../../../components/AppRadioBtn";
-import { db } from "../../../config/firebase-config";
-import { activeProductState } from "../../../atoms/productAtom";
-import { useRecoilValue } from "recoil";
-import { findIndex } from "lodash";
+import AppLayout from "../../../components/Dashboard/AppLayout"
+import {ActionFormCard, ActionFormCard2} from "../../../components/Dashboard/FormCard"
+import ItemCard from "../../../components/Dashboard/ItemCard"
+import {splitRoutes, timeScale} from "../../../utils"
+import MasonryGrid from "../../../components/Dashboard/MasonryGrid"
+import {RadioButtonWithFill} from "../../../components/AppRadioBtn"
+import {db} from "../../../config/firebase-config"
+import {activeProductState} from "../../../atoms/productAtom"
+import {useRecoilValue} from "recoil"
+import {findIndex} from "lodash"
 
-const names = ["Validated", "Assumed", "Disproven"];
+const names = ["Validated", "Assumed", "Disproven"]
 
 export default function Learnings() {
-  const { pathname } = useRouter();
+	const {pathname} = useRouter()
 
-  const activeProduct = useRecoilValue(activeProductState);
-  const [data, setData] = useState(null);
-  const [showAdd, setShowAdd] = useState(false);
+	const activeProduct = useRecoilValue(activeProductState)
+	const [data, setData] = useState(null)
+	const [showAdd, setShowAdd] = useState(false)
 
-  const [activeLearning, setActiveLearning] = useState(names[0]);
+	const [activeLearning, setActiveLearning] = useState(names[0])
 
-  const [learningName, setLearningName] = useState(activeLearning);
+	const [learningName, setLearningName] = useState(activeLearning)
 
-  const [temp, setTemp] = useState(null);
+	const [temp, setTemp] = useState(null)
 
-  // Fetch data from firebase
-  const fetchLearnings = async () => {
-    if (activeProduct) {
-      const res = db
-        .collection("Learnings")
-        .where("product_id", "==", activeProduct.id)
-        .where("type", "==", activeLearning)
-        .onSnapshot((snapshot) => {
-          setData(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-          console.log(
-            snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-          );
-        });
-    }
-  };
+	// Fetch data from firebase
+	const fetchLearnings = async () => {
+		if (activeProduct) {
+			const res = db
+				.collection("Learnings")
+				.where("product_id", "==", activeProduct.id)
+				.where("type", "==", activeLearning)
+				.onSnapshot((snapshot) => {
+					setData(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+					console.log(snapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
+				})
+		}
+	}
 
-  useEffect(() => {
-    fetchLearnings();
-  }, [activeProduct, activeLearning]);
+	useEffect(() => {
+		fetchLearnings()
+	}, [activeProduct, activeLearning])
 
-  const setLearning = (name, product) => {
-    const learningIndex = findIndex(names, (o) => o === name);
+	const setLearning = (name, product) => {
+		const learningIndex = findIndex(names, (o) => o === name)
 
-    setActiveLearning(names[learningIndex]);
-  };
+		setActiveLearning(names[learningIndex])
+	}
 
-  const addItem = () => {
-    setShowAdd(true);
-  };
+	const addItem = () => {
+		setShowAdd(true)
+	}
 
-  const addItemDone = (item) => {
-    console.log(item);
-    const data = {
-      name: item.title,
-      description: item.description,
-      type: activeLearning,
-      product_id: activeProduct.id,
-    };
-    db.collection("Learnings")
-      .add(data)
-      .then((docRef) => {
-        message.success("New item added successfully");
-      })
-      .catch((error) => {
-        message.error("Error adding item");
-      });
-    setShowAdd(false);
-  };
+	const addItemDone = (item) => {
+		console.log(item)
+		const data = {
+			name: item.title,
+			description: item.description,
+			type: activeLearning,
+			product_id: activeProduct.id,
+		}
+		db.collection("Learnings")
+			.add(data)
+			.then((docRef) => {
+				message.success("New item added successfully")
+			})
+			.catch((error) => {
+				message.error("Error adding item")
+			})
+		setShowAdd(false)
+	}
 
-  const editItem = async (id, item) => {
-    const data = temp
-      ? {
-          name: item.title,
-          description: item.description,
-          type: temp,
-        }
-      : {
-          name: item.title,
-          description: item.description,
-        };
+	const editItem = async (id, item) => {
+		console.log(temp)
 
-      console.log(id)
-      
+		const data = temp
+			? {
+					name: item.title,
+					description: item.description,
+					type: temp,
+			  }
+			: {
+					name: item.title,
+					description: item.description,
+			  }
 
-    await db
-      .collection("Learnings")
-      .doc(id)
-      .update(data)
-      .then(() => {
-        message.success("Learning updated successfully");
-        setTemp(null)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+		await db
+			.collection("Learnings")
+			.doc(id)
+			.update(data)
+			.then(() => {
+				message.success("Learning updated successfully")
+				if (temp) {
+					notification.info({
+						message: `Learning moved to new section`,
+						description: (
+							<p>
+								<span className="font-semibold text-[#C82D73]">{item.title}</span> moved to{" "}
+								<span className="font-semibold text-[#4A801D]">{temp}</span>
+							</p>
+						),
+						placement: "bottomRight",
+					})
+					setTemp(null)
+				}
+			})
+			.catch((err) => {
+				console.log(err)
+			})
+	}
 
-  const deleteItem = (id) => {
-    db.collection("Learnings").doc(id).delete();
-  }
+	const deleteItem = (id) => {
+		db.collection("Learnings").doc(id).delete()
+	}
 
-  // const rightNav = getNames(data[activeProduct]);
+	// const rightNav = getNames(data[activeProduct]);
 
-  return (
-    <div className="mb-8">
-      <Head>
-        <title>Dashboard | Sprint Zero</title>
-        <meta name="description" content="Sprint Zero strategy learnings" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className="mb-8">
+			<Head>
+				<title>Dashboard | Sprint Zero</title>
+				<meta name="description" content="Sprint Zero strategy learnings" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <AppLayout
-        rightNavItems={names}
-        activeRightItem={activeLearning}
-        setActiveRightNav={setLearning}
-        onMainAdd={addItem}
-        hasMainAdd
-        hasSideAdd={false}
-        mainClass="mr-[130px]"
-        breadCrumbItems={splitRoutes(pathname)}
-      >
-        <MasonryGrid>
-          {showAdd ? (
-            <ActionFormCard
-              headerSmall
-              className="mb-[16px]"
-              onCancel={() => setShowAdd(false)}
-              onSubmit={addItemDone}
-              isDisabled={true}
-              // extraItems={
-              //   <Radio.Group
-              //     className="mt-[12px] grid grid-cols-3"
-              //     size="small"
-              //   >
-              //     {names.map((opt, i) => (
-              //       <RadioButtonWithFill
-              //         key={i}
-              //         checked={opt === learningName}
-              //         onChange={() => setLearningName(opt)}
-              //         value={opt}
-              //       >
-              //         {opt}
-              //       </RadioButtonWithFill>
-              //     ))}
-              //   </Radio.Group>
-              // }
-            />
-          ) : null}
+			<AppLayout
+				rightNavItems={names}
+				activeRightItem={activeLearning}
+				setActiveRightNav={setLearning}
+				onMainAdd={addItem}
+				hasMainAdd
+				hasSideAdd={false}
+				mainClass="mr-[130px]"
+				breadCrumbItems={splitRoutes(pathname)}
+			>
+				<MasonryGrid>
+					{showAdd ? (
+						<ActionFormCard
+							headerSmall
+							className="mb-[16px]"
+							onCancel={() => setShowAdd(false)}
+							onSubmit={addItemDone}
+							isDisabled={true}
+							// extraItems={
+							//   <Radio.Group
+							//     className="mt-[12px] grid grid-cols-3"
+							//     size="small"
+							//   >
+							//     {names.map((opt, i) => (
+							//       <RadioButtonWithFill
+							//         key={i}
+							//         checked={opt === learningName}
+							//         onChange={() => setLearningName(opt)}
+							//         value={opt}
+							//       >
+							//         {opt}
+							//       </RadioButtonWithFill>
+							//     ))}
+							//   </Radio.Group>
+							// }
+						/>
+					) : null}
 
-          {data &&
-            data.map((res, i) => (
-              <ItemCard
-                extraItems={
-                  <Radio.Group
-                    className="mt-[12px] grid grid-cols-3"
-                    size="small"
-                  >
-                    {names.map((opt, i) => (
-                      <RadioButtonWithFill
-                        key={i}
-                        checked={opt === res.type}
-                        onChange={() => setTemp(opt)}
-                        value={opt}
-                      >
-                        {opt}
-                      </RadioButtonWithFill>
-                    ))}
-                  </Radio.Group>
-                }
-                // useBtn
-                key={res.id}
-                onEdit={(item) => editItem(res.id, item)}
-                item={res}
-                onDelete={() => deleteItem(res.id)}
-              />
-            ))}
-        </MasonryGrid>
-        {data?.length < 1 ? (
-          <>
-            {showAdd ? null : (
-              <div className="h-[600px] flex items-center justify-center">
-                <div
-                  style={{
-                    boxShadow:
-                      "0px 9px 28px 8px rgba(0, 0, 0, 0.05), 0px 6px 16px rgba(0, 0, 0, 0.08), 0px 3px 6px -4px rgba(0, 0, 0, 0.12)",
-                  }}
-                  className="w-[320px] h-[187px] bg-white flex items-center justify-center rounded"
-                >
-                  <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
-                </div>
-              </div>
-            )}
-          </>
-        ) : null}
-      </AppLayout>
-    </div>
-  );
+					{data &&
+						data.map((res, i) => (
+							<ItemCard
+								extraItems={
+									<Radio.Group className="mt-[12px] grid grid-cols-3" size="small">
+										{names.map((opt, i) => (
+											<RadioButtonWithFill key={i} checked={opt === res.type} onChange={() => setTemp(opt)} value={opt}>
+												{opt}
+											</RadioButtonWithFill>
+										))}
+									</Radio.Group>
+								}
+								// useBtn
+								key={res.id}
+								onEdit={(item) => editItem(res.id, item)}
+								item={res}
+								onDelete={() => deleteItem(res.id)}
+							/>
+						))}
+				</MasonryGrid>
+				{data?.length < 1 ? (
+					<>
+						{showAdd ? null : (
+							<div className="flex h-[600px] items-center justify-center">
+								<div
+									style={{
+										boxShadow:
+											"0px 9px 28px 8px rgba(0, 0, 0, 0.05), 0px 6px 16px rgba(0, 0, 0, 0.08), 0px 3px 6px -4px rgba(0, 0, 0, 0.12)",
+									}}
+									className="flex h-[187px] w-[320px] items-center justify-center rounded bg-white"
+								>
+									<Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+								</div>
+							</div>
+						)}
+					</>
+				) : null}
+			</AppLayout>
+		</div>
+	)
 }
