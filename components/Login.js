b77c206c7b13c605cf2faff3c06b8263bc6bc868
Adil Gurbanov <adil.gurban@gmail.com>
2023-01-14 22:37:36 +0100

    disabled user creation when logging in - added temp alert to be replaced with ant alert as per design

--- components/Login.js
+++ components/Login.js
@@ -1,27 +1,27 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect, useState} from "react"
-import {useRouter} from "next/router"
+import React, { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 // import Image from "next/image";
-import {Button, Typography, message, Image} from "antd"
-import {GoogleOutlined, WindowsFilled, AppleFilled} from "@ant-design/icons"
-import {googleProvider, microsoftProvider, appleProvider} from "../config/authMethods"
+import { Button, Typography, message, Image } from "antd"
+import { GoogleOutlined, WindowsFilled, AppleFilled } from "@ant-design/icons"
+import { googleProvider, microsoftProvider, appleProvider } from "../config/authMethods"
 import SocialMediaAuth from "../service/auth"
-import {auth} from "../config/firebase-config"
-import {db} from "../config/firebase-config"
-import {useAuth} from "../contexts/AuthContext"
+import { auth } from "../config/firebase-config"
+import { db } from "../config/firebase-config"
+import { useAuth } from "../contexts/AuthContext"
 // import { usePaymentConfirm } from "../contexts/PaymentContext";
 
-import {useGoogleReCaptcha} from "react-google-recaptcha-v3"
-import {activeProductState} from "../atoms/productAtom"
-import {useRecoilValue, useRecoilState} from "recoil"
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3"
+import { activeProductState } from "../atoms/productAtom"
+import { useRecoilValue, useRecoilState } from "recoil"
 
-const {Title, Text} = Typography
+const { Title, Text } = Typography
 
 const Login = () => {
 	const router = useRouter()
-	const {user} = useAuth()
-	const {type, product} = router.query
-	const {executeRecaptcha} = useGoogleReCaptcha()
+	const { user } = useAuth()
+	const { type, product } = router.query
+	const { executeRecaptcha } = useGoogleReCaptcha()
 	// const paid = usePaymentConfirm();
 	// const [activeProduct, setActiveProduct] = useState();
 	const [activeProduct, setActiveProduct] = useRecoilState(activeProductState)
@@ -29,10 +29,10 @@ const Login = () => {
 	const fetchProducts = async (user) => {
 		if (user) {
 			const _userObj = await db.collection("Users").doc(user.uid).get()
-			const userObj = {id: _userObj.id, ..._userObj.data()}
+			const userObj = { id: _userObj.id, ..._userObj.data() }
 
 			const _products = userObj.products.map(productId => db.collection("Products").doc(productId).get())
-			const products = (await Promise.all(_products)).map(doc => ({id: doc.id, ...doc.data()}))
+			const products = (await Promise.all(_products)).map(doc => ({ id: doc.id, ...doc.data() }))
 			return products[0]
 		}
 	}
@@ -56,12 +56,13 @@ const Login = () => {
 				const userRef = db.collection("Users").doc(user.uid)
 				const doc = await userRef.get()
 				if (!doc.exists) {
-					userRef.set({
-						avatar: user.photoURL,
-						email: user.email,
-						name: user.displayName,
-						products: [],
-					})
+					alert("You are not signed up for the Beta - temp alert to be replaced")
+					// userRef.set({
+					// 	avatar: user.photoURL,
+					// 	email: user.email,
+					// 	name: user.displayName,
+					// 	products: [],
+					// })
 				}
 
 				await fetchProducts(user).then(async (product) => {
@@ -103,7 +104,7 @@ const Login = () => {
 		<>
 			<div className="mb-4 flex items-center justify-center">
 				<div>
-					<Title level={1} style={{fontWeight: "normal"}}>
+					<Title level={1} style={{ fontWeight: "normal" }}>
 						Authenticate Yourself Before You Wreck Yourself
 					</Title>
 					<Text className="text-left text-xl">Select a provider to login</Text>
@@ -114,7 +115,7 @@ const Login = () => {
 				<Button
 					className="flex h-[54px] w-[345px] items-center justify-start space-x-4 rounded-[10px] border-black bg-white text-[20px] font-semibold"
 					onClick={() => handleOnClick(appleProvider)}
-					icon={<AppleFilled style={{fontSize: "20px", marginTop: "-5px"}} />}
+					icon={<AppleFilled style={{ fontSize: "20px", marginTop: "-5px" }} />}
 				>
 					<p>Sign in with Apple</p>
 				</Button>
