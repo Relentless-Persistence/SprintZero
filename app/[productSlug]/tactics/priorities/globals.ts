760916061165fa0b384d79452f15e8bc9dd69cf9
Brandon Tsang <brandononline2@gmail.com>
2023-01-20 11:23:52 -0500

    fix error

--- app/[productSlug]/tactics/priorities/globals.ts
+++ app/[productSlug]/tactics/priorities/globals.ts
@@ -1,18 +1,20 @@
+/* eslint-disable no-var */
+
 declare global {
-	interface Window {
-		__pointerLocation: {current: [number, number]}
-		__matrixRect: {current: DOMRect}
-	}
+	var __pointerLocation: {current: [number, number]}
+	var __matrixRect: {current: DOMRect}
 }
 
 if (typeof window !== `undefined`) {
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	window.__pointerLocation = window.__pointerLocation ?? {current: [0, 0]}
+	globalThis.__pointerLocation = globalThis.__pointerLocation ?? {current: [0, 0]}
 	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-	window.__matrixRect = window.__matrixRect ?? {
+	globalThis.__matrixRect = globalThis.__matrixRect ?? {
 		current: {x: 0, y: 0, width: 1, height: 1, top: 0, right: 1, bottom: 1, left: 0},
 	}
 }
 
-export let pointerLocation = window.__pointerLocation
-export let matrixRect = window.__matrixRect
+export let pointerLocation = globalThis.__pointerLocation ?? {current: [0, 0]}
+export let matrixRect = globalThis.__matrixRect ?? {
+	current: {x: 0, y: 0, width: 1, height: 1, top: 0, right: 1, bottom: 1, left: 0},
+}
