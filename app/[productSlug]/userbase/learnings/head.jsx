38669734985b3d2ca71286c3da1b8c1878d71cfd
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2023-01-25 00:34:52 +0100

    Moved learnings and updated UI

---
+++ app/[productSlug]/userbase/learnings/head.jsx
@@ -0,0 +1,10 @@
+const Head = () => {
+	return (
+		<>
+			<title>Learnings | SprintZero</title>
+			<meta name="description" content="Learnings" />
+		</>
+	)
+}
+
+export default Head
--- components/Dashboard/ItemCard.js
+++ components/Dashboard/ItemCard.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { Button, Card } from "antd5";
 
 import CardHeaderButton, { CardHeaderLink } from "./CardHeaderButton";
-import FormCard, { ActionFormCard, ObjectiveActionFormCard } from "./FormCard";
+import FormCard, { ActionFormCard, ObjectiveActionFormCard, LearningsActionFormCard } from "./FormCard";
 import { CardTitle as Title } from "./CardTitle";
 import { useAuth } from "../../contexts/AuthContext"
 
@@ -171,5 +171,83 @@ export const ObjectiveItemCard = ({
     </>
   );
 };
+
+export const LearningsItemCard = ({
+	onEdit,
+	useBtn,
+	item,
+	itemBtnText = "Edit",
+	version = 2,
+	headerSmall = false,
+	extraItems,
+	index,
+	onDelete,
+}) => {
+	const userRole = "member"
+	const [isEdit, setIsEdit] = useState(false)
+
+	const toggleEdit = () => setIsEdit((s) => !s)
+
+	const handleEdit = (item) => {
+		onEdit(item)
+		toggleEdit()
+	}
+
+	if (isEdit) {
+		return (
+			<LearningsActionFormCard
+				headerSmall={headerSmall}
+				extraItems={extraItems}
+				id={item?.id}
+				title={item.name}
+				description={item.description}
+        artifact={item.artifact}
+				useAction={true}
+				version={version}
+				onSubmit={handleEdit}
+				onCancel={toggleEdit}
+				className="mb-[16px] border-2 border-[#D9D9D9]"
+				onDelete={onDelete}
+			/>
+		)
+	}
+
+	return (
+		<>
+			{userRole && (
+				<Card
+					// $headerSmall={headerSmall}
+					className="mb-[16px] border border-[#D9D9D9]"
+					type="inner"
+					extra={
+						userRole !== "viewer" ? (
+							useBtn ? (
+								<CardHeaderButton size="small" onClick={toggleEdit}>
+									{itemBtnText}
+								</CardHeaderButton>
+							) : (
+								<CardHeaderLink size="small" onClick={toggleEdit}>
+									Edit
+								</CardHeaderLink>
+							)
+						) : null
+					}
+					title={<Title>{item.name || `Result #${index}`}</Title>}
+					headStyle={{
+						background: "#F5F5F5",
+					}}
+				>
+					<p>{item.description}</p>
+
+					<div className="mt-4 flex items-center justify-end">
+						<a href={item.artifact} target="_blank">
+							<Button className="mt-2 rounded-[2px] border-[#FF4D4F] text-[#FF4D4F]">View Artifact</Button>
+						</a>
+					</div>
+				</Card>
+			)}
+		</>
+	)
+}
 
 export default ItemCard;
