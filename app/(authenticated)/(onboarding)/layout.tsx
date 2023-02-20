bbce53f6e7092c4d90f06e57e2bd9cbae5d3305a
Brandon Tsang <brandononline2@gmail.com>
2023-02-20 15:16:04 -0500

    update tos page

--- app/(authenticated)/(onboarding)/layout.tsx
+++ app/(authenticated)/(onboarding)/layout.tsx
@@ -5,6 +5,7 @@ import Image from "next/image"
 
 import type {FC, ReactNode} from "react"
 
+import LinkTo from "~/components/LinkTo"
 import {useUser} from "~/utils/useUser"
 
 export type OnboardingLayoutProps = {
@@ -17,17 +18,27 @@ const OnboardingLayout: FC<OnboardingLayoutProps> = ({children}) => {
 	return (
 		<div className="h-full w-full overflow-x-hidden">
 			<div className="mx-auto flex h-full max-w-5xl flex-col gap-8 p-8">
-				<div className="flex justify-between">
-					<Image src="/images/logo_beta_light.png" alt="SprintZero logo" width={178} height={42} priority />
-					<div className="flex items-center gap-2">
-						<Avatar src={user?.data().avatar} size="large" alt="Avatar" className="border border-black" />
-						<div className="flex w-min flex-col gap-1">
-							<p className="font-semibold">{user?.data().name}</p>
-							<p className="text-ellipsis text-gray">{user?.data().email}</p>
+				<div className="flex items-center justify-between gap-4">
+					<Image src="/images/logo_beta_light.png" alt="SprintZero logo" width={238} height={56} priority />
+					<div className="flex min-w-0 flex-1 flex-col items-end gap-1">
+						<div className="flex w-full flex-1 items-center gap-3">
+							<div className="min-w-0 flex-1 text-end">
+								<p>{user?.data().name}</p>
+								<p className="truncate text-sm text-gray">{user?.data().email}</p>
+							</div>
+							<Avatar
+								src={user?.data().avatar}
+								size={48}
+								alt="Avatar"
+								className="shrink-0 basis-auto border border-[#a7c983]"
+							/>
 						</div>
+						<LinkTo href="/sign-out" className="text-sm text-blue">
+							Sign out
+						</LinkTo>
 					</div>
 				</div>
-				{children}
+				<div className="grow">{children}</div>
 			</div>
 		</div>
 	)
