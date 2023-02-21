2f3f57c4b1f64f885f8380a8d0fd17fb365ef6f3
Brandon Tsang <brandononline2@gmail.com>
2023-02-21 16:30:59 -0500

    update fun card api stuff

--- app/(authenticated)/(mainApp)/[productSlug]/operations/huddle/FunCard.tsx
+++ app/(authenticated)/(mainApp)/[productSlug]/operations/huddle/FunCard.tsx
@@ -50,7 +50,7 @@ const FunCard: FC = () => {
 		}
 
 		const formattedDate = date.format(`MMMM D, YYYY`)
-		const gptQuestion = `What was the #1 song on the Billboard Top 100 list on ${formattedDate}? Give in the format "Artist - name".`
+		const gptQuestion = `What was the #1 song on the Billboard Top 100 list on ${formattedDate}? Give in the format "Song name - Artist".`
 		const res = await axios.post(`/api/gpt`, {prompt: gptQuestion})
 
 		const {response} = z.object({response: z.string()}).parse(res.data)
@@ -73,18 +73,9 @@ const FunCard: FC = () => {
 	}
 
 	const getSong = async (songString: string) => {
-		const newString = songString
-			.trimStart()
-			.replace(/'/g, `"`)
-			.replace(/(\w+:)|"|{|}/g, ``)
-			.trim()
-		const obj = newString
-			.split(`,`)
-			.map((s) => s.trim().replace(/ /g, `+`))
-			.join(`+`)
-
+		const newString = songString.replace(/^"/, ``).replace(/"$/, ``)
 		const res = await axios.post<Result>(`/api/fetchSong`, {
-			song: encodeURIComponent(obj),
+			song: encodeURIComponent(newString),
 		})
 		const oldUrl = res.data.results.songs.data[0].attributes.url
 		const domainIndex = oldUrl.indexOf(`music.apple.com`)
