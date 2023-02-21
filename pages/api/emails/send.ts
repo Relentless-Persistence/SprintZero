3705befbbcd6e9199aaed4127afb3908edb3fef1
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2023-02-21 15:24:11 +0100

    Fix email type errors

--- pages/api/emails/send.ts
+++ pages/api/emails/send.ts
@@ -1,6 +1,6 @@
-import {NextApiRequest, NextApiResponse} from "next"
 import nodemailer from "nodemailer"
 
+import type {NextApiHandler, NextApiRequest, NextApiResponse} from "next"
 interface EmailRequest {
 	to: string
 	from: string
@@ -9,27 +9,26 @@ interface EmailRequest {
 	attachments?: string[]
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-	if (req.method !== "POST") {
-		return res.status(405).json({message: "Method not allowed"})
+const handler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method !== `POST`) {
+		return res.status(405).json({message: `Method not allowed`})
 	}
 
-	const {to, from, subject, body}: EmailRequest = req.body
+	const {to, from, subject, body} = req.body as EmailRequest
 
 	let emailPassword: string
 	if (from === process.env.EMAIL_FROM_NO_REPLY) {
-		emailPassword = process.env.EMAIL_PASSWORD_NO_REPLY
+		emailPassword = process.env.EMAIL_PASSWORD_NO_REPLY || ``
 	} else {
-		return res.status(401).json({message: "Invalid from email"})
+		return res.status(401).json({message: `Invalid from email`})
 	}
 
-	console.log("From " + req.body)
 	const userEmail = from
 	const userPassword = emailPassword
 
 	// Create a transporter using Gmail SMTP
 	const transporter = nodemailer.createTransport({
-		service: "gmail",
+		service: `gmail`,
 		auth: {
 			user: userEmail,
 			pass: userPassword,
@@ -48,10 +47,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 		await transporter.sendMail(mailOptions)
 
 		// Send a success response
-		res.status(200).json({message: "Email sent successfully."})
+		res.status(200).json({message: `Email sent successfully.`})
 	} catch (error) {
 		// Send an error response
-		res.status(500).json({message: "Internal server error" + error.message})
+		res.status(500).json(error || `Internal Server error`)
 	}
 }
 
