0f38d6c32ce86e1b222a9d1196fca5b1bde1bb02
Adil Gurbanov <adil.gurban@gmail.com>
2023-03-01 13:22:32 +0100

    firebase admin - version 1

--- src/pages/api/map-user-to-product.ts
+++ src/pages/api/map-user-to-product.ts
@@ -1,14 +1,30 @@
-import {collection, doc, getDoc, getDocs, query, setDoc, updateDoc, where} from "firebase/firestore"
+import {collection, doc, getDoc, getDocs, getFirestore, query, setDoc, updateDoc, where} from "firebase/firestore"
 import admin from "firebase-admin"
+import firestore from "firebase-admin"
 
 import type {NextApiHandler} from "next"
 
 import {ProductConverter} from "~/types/db/Products"
+import {dBa} from "~/utils/firebase"
 let serviceAccount = require(`../../keys/firebase-admin-sa-key.json`)
 
-admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-})
+// // const app = admin.initializeApp({
+// // 	credential: admin.credential.cert(serviceAccount),
+// // })
+
+// admin.initializeApp(
+// 	{
+// 		credential: admin.credential.cert(serviceAccount),
+// 	},
+// 	`dBa`,
+// )
+
+// const db = admin.firestore()
+
+//const db = admin.firestore()
+//const db = getFirestore(`dBa`)
+//const db = getFirestore(adminApp)
+//export const db = getFirestore(app)
 
 // You can now use Firestore with the authenticated admin user
 //const firestore = admin.firestore()
@@ -23,7 +39,7 @@ const handler: NextApiHandler = async (req, res) => {
 	const {productId, userId, role} = req.body as AddMemberRequest
 
 	try {
-		const productRef = doc(dbAdmin, `Products`, productId).withConverter(ProductConverter)
+		const productRef = doc(dBa, `Products`, productId).withConverter(ProductConverter)
 		await updateDoc(productRef, {
 			[`members.${userId}`]: {type: role},
 		})
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,6 +1,7 @@
 import {initializeApp} from "firebase/app"
 import {GithubAuthProvider, GoogleAuthProvider, OAuthProvider, getAuth} from "firebase/auth"
 import {connectFirestoreEmulator, getFirestore} from "firebase/firestore"
+import admin from "firebase-admin"
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
@@ -15,6 +16,7 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+// export const dBa = admin.firestore()
 if (process.env.NODE_ENV === `development` && process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_PORT !== undefined)
 	connectFirestoreEmulator(db, `localhost`, parseInt(process.env.NEXT_PUBLIC_FIREBASE_EMULATOR_PORT))
 
