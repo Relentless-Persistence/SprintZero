92d272509b8ca5299a430d6243670d77793928af
Brandon Tsang <brandononline2@gmail.com>
2023-03-02 15:10:15 -0500

    update beta user list

--- src/utils/betaUserList.ts
+++ src/utils/betaUserList.ts
@@ -5,7 +5,10 @@ export const betaUsers = [
 	`obafemi@relentlesspersistenceinc.com`,
 
 	`adil.gurban@gmail.com`,
+	`boldingbroke@gmail.com`,
 	`brandononline2@gmail.com`,
+	`chrislouvion@gmail.com`,
 	`femzytest@gmail.com`,
+	`herrema.drew@gmail.com`,
 	`mvoshell@gmail.com`,
 ]
