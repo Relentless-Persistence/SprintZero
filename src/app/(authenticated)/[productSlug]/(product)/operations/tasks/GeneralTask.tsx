3518a56e589a411fc175f20338272ce6ce0a69ed
obafemi-rpi <obafemi@relentlesspersistenceinc.com>
2023-04-30 01:47:41 +0100

    Fix type errors

--- src/app/(authenticated)/[productSlug]/(product)/operations/tasks/GeneralTask.tsx
+++ src/app/(authenticated)/[productSlug]/(product)/operations/tasks/GeneralTask.tsx
@@ -1,11 +1,11 @@
 
-import { useState } from "react"
+// import { useState } from "react"
 
 import type { FC } from "react"
 import type { Task } from "~/types/db/Products/Tasks"
 
 import TaskColumn from "./TaskColumn"
-import { useAppContext } from "~/app/(authenticated)/[productSlug]/AppContext"
+// import { useAppContext } from "~/app/(authenticated)/[productSlug]/AppContext"
 
 interface MyTask extends Task {
 	id: string
@@ -16,8 +16,6 @@ interface Props {
 }
 
 const GeneralTask: FC<Props> = ({ tasks }) => {
-	const { product } = useAppContext()
-	const [editingTask, setEditingTask] = useState(false)
 
 	return (
 		<div className="">
@@ -25,10 +23,10 @@ const GeneralTask: FC<Props> = ({ tasks }) => {
 			<div className="h-full grid grow auto-cols-[286px] grid-flow-col gap-4">
 
 
-				<TaskColumn id="todo" title="To Do" tasks={tasks} onEdit={() => setEditingTask(true)} />
-				<TaskColumn id="inProgress" title="In Progress" tasks={tasks} onEdit={() => setEditingTask(true)} />
-				<TaskColumn id="review" title="Review" tasks={tasks} onEdit={() => setEditingTask(true)} />
-				<TaskColumn id="done" title="Done" tasks={tasks} onEdit={(id) => setEditingTask(true)} />
+				<TaskColumn id="todo" title="To Do" tasks={tasks} />
+				<TaskColumn id="inProgress" title="In Progress" tasks={tasks} />
+				<TaskColumn id="review" title="Review" tasks={tasks} />
+				<TaskColumn id="done" title="Done" tasks={tasks} />
 
 			</div>
 		</div>
