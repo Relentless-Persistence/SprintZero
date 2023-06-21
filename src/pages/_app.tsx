481bdda1b5d786df3431b68012479882a2d54ab3
Adil Gurbanov <adilg@Adils-MBP.home>
2023-06-21 22:55:58 +0200

    enabled google analytics for production environment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,9 +6,24 @@ import type { ReactElement } from "react"
 import "../app/styles.css"
 
 const CustomApp = ({ Component, pageProps }: AppProps): ReactElement | null => {
-	return (
+	if (process.env.NODE_ENV !== `production`) {
+		return <Component {...pageProps} />
+	}
 
-		<Component {...pageProps} />
+	return (
+		<>
+			<Script
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+			/>
+			<Script id="google-analytics" strategy="afterInteractive">
+				{`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());` +
+					`gtag('config', '` + String(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID) + `');`}
+			</Script>
+			<Component {...pageProps} />
+		</>
 	)
 }
 
